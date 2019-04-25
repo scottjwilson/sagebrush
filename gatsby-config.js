1904c98d38966ecfae471ae7ad8c57334d246596
scottjwilson <slimzel@protonmail.com>
2019-04-25 12:11:22 -0700

    updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,6 @@ module.exports = {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        host: `preview.contentful.com`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
