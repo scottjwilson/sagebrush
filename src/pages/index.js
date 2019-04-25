0b7b03a5ae559a3c6907fed252f3ed5dce1a95d8
scottjwilson <slimzel@protonmail.com>
2019-04-24 20:18:52 -0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,17 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import { FaCartPlus } from "react-icons/fa"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <h1>
+      <FaCartPlus />
+    </h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
