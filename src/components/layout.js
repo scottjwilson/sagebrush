06cd88c4f3958ab0b9315ca00f01c9035d56463b
scottjwilson <slimzel@protonmail.com>
2019-04-24 23:09:24 -0700

    Added Navbar component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,10 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import Navbar from "../components/Globals/Navbar"
 
-import Header from "./header"
-import "./layout.css"
+import "./bootstrap.min.css"
+import "../components/layout.css"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -25,22 +19,13 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+        <Navbar />
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
       </>
     )}
   />
